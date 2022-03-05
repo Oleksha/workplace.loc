a7355f1d8acef259ceadf84db6dfd694109d7d8d
Oleksha <43583583+Oleksha@users.noreply.github.com>
2022-03-05 09:18:37 +0400

    Update index.php

--- app/views/Main/index.php
+++ app/views/Main/index.php
@@ -41,6 +41,7 @@
                             $pay = 'Нет данных';
                         }
                     }
+                  
                   ?>
                   <tr>
                       <th><a href="partner/<?= $item['inn'];?>"><?= $item['partner'];?></a></th>
@@ -51,7 +52,7 @@
                       </td>
                       <td><?= $status;?></td>
                       <td>
-                          <?php if ($status == 'Подано на оплату') : ?>
+                        <?php if ($status == 'Подано на оплату') : ?>
                             <a class="btn btn-outline-success payment_pay_link" data-toggle="tooltip" data-placement="top" data-id_receipt="<?= $item['id_receipt'];?>">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cash-coin" viewBox="0 0 16 16">
                                     <path fill-rule="evenodd" d="M11 15a4 4 0 1 0 0-8 4 4 0 0 0 0 8zm5-4a5 5 0 1 1-10 0 5 5 0 0 1 10 0z"/>
@@ -60,12 +61,12 @@
                                     <path d="M9.998 5.083 10 5a2 2 0 1 0-3.132 1.65 5.982 5.982 0 0 1 3.13-1.567z"/>
                                 </svg>
                             </a>
-                          <?php endif; ?>
-                          <button type="button" class="btn btn-outline-warning" data-toggle="tooltip" data-placement="top" title="Изменить" data-id_receipt="<?= $item['id_receipt'];?>">
+                        <?php endif; ?>
+                        <button type="button" class="btn btn-outline-warning" data-toggle="tooltip" data-placement="top" title="Изменить" data-id_receipt="<?= $item['id_receipt'];?>">
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil" viewBox="0 0 16 16">
                                 <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z"></path>
                             </svg>
-                          </button>
+                        </button>
                       </td>
                   </tr>
               <?php endforeach; ?>
