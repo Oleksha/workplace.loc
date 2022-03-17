6fe82328571c9a277628c42499abfa0db0fad6b9
Oleksha <43583583+Oleksha@users.noreply.github.com>
2022-03-17 13:17:19 +0400

    Добавлен расход по БО

--- app/controllers/BudgetController.php
+++ app/controllers/BudgetController.php
@@ -8,13 +8,51 @@ class BudgetController extends AppController {
         $year = isset($_GET['y']) ? $_GET['y'] : date('Y');
         $month = isset($_GET['m']) ? $_GET['m'] : date('m');
         $budget_date = $year . '-' . $month . '-01';
-        //debug($budget_date);die;
         // получение БО из БД
         $budgets = \R::find('budget', "WHERE scenario = '{$budget_date}' ORDER BY scenario, number");
         // формируем метатеги для страницы
         $this->setMeta('Список бюджетных операций', 'Описание...', 'Ключевые слова...');
+        // получаем расходы по выбранным БО
+        foreach ($budgets as $item) {
+            // получаем номер БО 
+            $num_bo = $item['number'].'/'.$year;
+            $payments = \R::find('payment', "num_bo LIKE '%{$num_bo}%'");
+            $item['payment'] = $this->get_sum($payments, $num_bo, $item['vat']);
+        }
         // Передаем полученные данные в вид
         $this->set(compact('budgets', 'year', 'month'));
+    }
+
+    private function get_sum($payments, $num_bo, $vat_bo) {
+        $sum = 0.00;
+        foreach ($payments as $payment) {
+            $nums = explode(';', trim($payment->num_bo));
+            $sums = explode(';', trim($payment->sum_bo));
+            $key = array_search($num_bo, $nums);
+            if ($vat_bo == '1.20') {
+                // если БО с НДС
+                if ($payment->vat == '1.20') {
+                    // если платеж с НДС
+                    $sum += $sums[$key];
+                }
+                if ($payment->vat == '1.00') {
+                    // если платеж без НДС
+                    $sum += round($sums[$key] * 1.2, 2);
+                }
+            }
+            if ($vat_bo == '1.00') {
+                // если БО без НДС
+                if ($payment->vat == '1.00') {
+                    // если платеж без НДС
+                    $sum += $sums[$key];
+                }
+                if ($payment->vat == '1.20') {
+                    // если платеж с НДС
+                    $sum += round($sums[$key] / 1.2, 2);
+                }
+            }            
+        }
+        return $sum;
     }
 
 }
