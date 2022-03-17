705c9827995f8a33c40defedd959f2be2890c946
Oleksha <43583583+Oleksha@users.noreply.github.com>
2022-03-17 13:18:18 +0400

    Добавлен расход по БО

--- app/views/Budget/index.php
+++ app/views/Budget/index.php
@@ -57,6 +57,8 @@
                     <th>МО</th>
                     <th>Номер</th>
                     <th>Сумма</th>
+                    <th>Оплачено</th>
+                    <th>Остаток</th>
                     <th>НДС</th>
                     <th>Статья</th>
                 </tr>
@@ -67,22 +69,24 @@
                         <?php 
                             $date = date_create($budget->scenario);
                             $_monthsList = array(
-                                "1"=>"Январь","2"=>"Февраль","3"=>"Март",
-                                "4"=>"Апрель","5"=>"Май", "6"=>"Июнь",
-                                "7"=>"Июль","8"=>"Август","9"=>"Сентябрь",
-                                "10"=>"Октябрь","11"=>"Ноябрь","12"=>"Декабрь");
+                                "1"=>"Янв","2"=>"Фвр","3"=>"Мрт",
+                                "4"=>"Апр","5"=>"Май", "6"=>"Июн",
+                                "7"=>"Июл","8"=>"Авг","9"=>"Сен",
+                                "10"=>"Окт","11"=>"Ноя","12"=>"Дек");
                                  
                             $scenario = $_monthsList[date_format($date, "n")].'&nbsp;'.date_format($date, "Y");
                             $date = date_create($budget->month_exp);
-                            $month_exp = $_monthsList[date_format($date, "n")].'&nbsp;'.date_format($date, "Y");
+                            $month_exp = $_monthsList[date_format($date, "n")];//.'&nbsp;'.date_format($date, "Y");
                             $date = date_create($budget->month_pay);
-                            $month_pay = $_monthsList[date_format($date, "n")].'&nbsp;'.date_format($date, "Y");
+                            $month_pay = $_monthsList[date_format($date, "n")];//.'&nbsp;'.date_format($date, "Y");
                         ?>
                         <td><?= $scenario;?></td><!--<a href="partner/<?= $partner->inn;?>"><?= $partner->name;?></a>-->
                         <td><?= $month_exp;?></td>
                         <td><?= $month_pay;?></td>
-                        <td><?= $budget->number;?></td>
+                        <th><?= $budget->number;?></th>
                         <td><?= number_format($budget->summa, 2, ',', '&nbsp;');?>&nbsp;₽</td>
+                        <td><?= number_format($budget->payment, 2, ',', '&nbsp;');?>&nbsp;₽</td>
+                        <th><?= number_format($budget->summa - $budget->payment, 2, ',', '&nbsp;');?>&nbsp;₽</th>
                         <td><?= $budget->vat;?></td>                        
                         <td><?= $budget->budget_item;?></td>
                     </tr>
