24c2a00fffc740c5983adaa4fea0024dcd3823d6
Oleksha <deryabino@gmail.com>
2022-03-31 21:18:05 +0400

    Ревизия программного кода

--- app/controllers/ReceiptController.php
+++ app/controllers/ReceiptController.php
@@ -9,6 +9,7 @@ use RedBeanPHP\R;
 class ReceiptController extends AppController {
 
     /**
+     * Функция редактирования данных о приходе
      * @return void
      */
     public function editAction() {
@@ -30,6 +31,10 @@ class ReceiptController extends AppController {
         redirect();
     }
 
+    /**
+     * Функция изменения данных о приходе в БД после редактирования
+     * @return void
+     */
     public function editReceiptAction() {
         // получаем данные пришедшие методом POST
         $edit_receipt = !empty($_POST) ? $_POST : null;
@@ -39,22 +44,25 @@ class ReceiptController extends AppController {
         redirect();
     }
 
+    /**
+     * Функция добавления нового прихода
+     * @return void
+     */
     public function addAction() {
-        // получаем данные пришедшие методом POST
-        // получаем переданное наименование КА
-        $partner = !empty($_GET['partner']) ? $_GET['partner'] : null;
-        $vat = !empty($_GET['vat']) ? $_GET['vat'] : null;
-        //debug($vat);die;
-        $receipt = new Receipt();
-        $receipt->addReceipt($partner);
-        $_SESSION['receipt']['vat'] = $vat;
+        // получаем данные пришедшие методом GET
+        $partner = !empty($_GET['partner']) ? $_GET['partner'] : null; // Наименование КА
+        $vat = !empty($_GET['vat']) ? $_GET['vat'] : null; // Ставка НДС: 1.20 - 20%, 1.00 - без НДС
         if ($this->isAjax()) {
             // Если запрос пришел АЯКСом
-            $this->loadView('receipt_add_modal');
+            $this->loadView('receipt_add_modal', compact('partner', 'vat'));
         }
         redirect();
     }
 
+    /**
+     * Функция добавления данных о новом приходе в БД
+     * @return void
+     */
     public function addReceiptAction() {
         // получаем данные пришедшие методом POST
         $add_receipt = !empty($_POST) ? $_POST : null;
@@ -64,8 +72,12 @@ class ReceiptController extends AppController {
         redirect();
     }
 
+    /**
+     * Функция удаления выбранного прихода
+     * @return void
+     */
     public function delAction() {
-        // получаем переданный идентификатор ЕР
+        // получаем переданный идентификатор прихода
         $id = !empty($_GET['id']) ? (int)$_GET['id'] : null;
         if ($id) {
             R::hunt('receipt', 'id = ?', [$id]);
@@ -73,13 +85,17 @@ class ReceiptController extends AppController {
         redirect();
     }
 
+    /**
+     * Функция Добавления или редактирования ЗО прихода
+     * @return void
+     */
     public function payAction() {
         // получаем переданные GET данные
         $id = !empty($_GET['id']) ? (int)$_GET['id'] : null;                    // идентификатор прихода  
         $vat = !empty($_GET['vat']) ? $_GET['vat'] : null;                      // ставка НДС по которой работает КА
         $partner_id = !empty($_GET['partner']) ? (int)$_GET['partner'] : null;  // идентификатор контрагента
-        $receipt = \R::findOne('receipt', 'id = ?', [$id]);                     // получаем полные данные о текущем приходе
-        $pay_key = !is_null($receipt->date_pay);                 // индикатор оплаты прихода
+        $receipt = \R::findOne('receipt', 'id = ?', [$id]);            // получаем полные данные о текущем приходе
+        $pay_key = !is_null($receipt->date_pay);                                // индикатор оплаты прихода
         /***** Начало получения данных для формирования заявки на оплату (ЗО) ******/
         /* Получаем все действующие ЕР для этого КА на момент прихода */
         $ers = \R::getAll("SELECT er.*, budget_items.name_budget_item FROM er, budget_items WHERE (budget_items.id = er.id_budget_item) AND (data_start <= '{$receipt->date}') AND (data_end >= '{$receipt->date}') AND id_partner = ?", [$partner_id]);
@@ -90,11 +106,10 @@ class ReceiptController extends AppController {
         }
         $ers = $er;
         /* Проверяем есть ли у этого прихода завка на оплату (ЗО) */
-        $name = $receipt->partner;                                              // Имя КА (ВСС ООО)
-        $year = date('Y', strtotime($receipt->date));                           // Получаем год прихода (2022)
-        $receipt_num = '%' . $receipt->number . '/' . $year . '%';              // Получаем используемый номер прихода (TOF00000000/2022)
+        $name = $receipt->partner;                                 // Имя КА (ВСС ООО)
+        $year = date('Y', strtotime($receipt->date));       // Получаем год прихода (2022)
+        $receipt_num = '%' . $receipt->number . '/' . $year . '%'; // Получаем используемый номер прихода (TOF00000000/2022)
         $payments = \R::findOne('payment', "receipt LIKE ?", [$receipt_num]);   // Получаем заявку на оплату для этого прихода (если есть)
-        
         $receipts = \R::find('receipt', 'partner = ? AND date_pay IS NULL ORDER BY date', [$name]); // Получаем все неоплаченные приходы этого КА
         /***************** Получаем массив приходов в зависимости от режима
         Array (пример)
@@ -118,11 +133,11 @@ class ReceiptController extends AppController {
                 )   
         ) *****************************************************************/
         $receipt_select = []; // массив содержащий выбранные приходы в ЗО
-        $receipt_nopay = [];  // массив содержащий неоплаченные приходы данного КА
-        $ers_sel = []; $new_er = []; $new_sums = [];
+        $receipt_no_pay = [];  // массив содержащий неоплаченные приходы данного КА
+        $ers_sel = []; $new_er = []; $new_sums = []; $new_recs = [];
         foreach ($receipts as $k => $v) {
-            $receipt_nopay[$k]['number'] = dateYear($v->number, $v->date);
-            $receipt_nopay[$k]['summa'] = $v->sum;
+            $receipt_no_pay[$k]['number'] = dateYear($v->number, $v->date);
+            $receipt_no_pay[$k]['summa'] = $v->sum;
         }
         if (!$payments) {
             // Если ЗО нет (режим добавления)
@@ -132,92 +147,88 @@ class ReceiptController extends AppController {
             $er_sel = explode(';', $payments->num_er); // выбранные ер
             $er_sum = explode(';', $payments->sum_er); // суммы выбранных ер
             foreach ($er_sel as  $k => $v) {
-                $new_er[$k]['number'] = $er_sel[$k];
+                $new_er[$k]['number'] = $v;
                 $new_er[$k]['summa'] = $er_sum[$k];
             }
             $ers_sel = $new_er;
             $recs = explode(';', $payments->receipt); // доступные приходы
             $sums = explode(';', $payments->sum); // все выбранные приходы
             foreach ($recs as  $k => $v) {
-                $new_recs[$k]['number'] = $recs[$k];
+                $new_recs[$k]['number'] = $v;
                 $new_recs[$k]['summa'] = $sums[$k];
             }
             $receipt_select = $new_recs;
             if ($pay_key) {
                 // Если ЗО создана и уже оплачена (режим просмотра)
-                $receipt_nopay = $new_recs;
+                $receipt_no_pay = $new_recs;
             } else {
                 // Если ЗО создана но пока не оплачена (режим редактирования)
                 foreach ($receipts as $k => $v) {
                     $new_sums[$k]['number'] = dateYear($v->number, $v->date);
                     $new_sums[$k]['summa'] = $v->sum;
                 }
-                $receipt_nopay = $new_sums;  
+                $receipt_no_pay = $new_sums;
             }
         }
         /***** Конец получения данных для формирования заявки на оплату ******/
         //debug($ers);die;
         if ($this->isAjax()) {
             // Если запрос пришел АЯКСом
-            $this->loadView('payment_add_modal', compact('name', 'receipt_select', 'receipt_nopay', 'ers', 'ers_sel', 'payments', 'vat'));
+            $this->loadView('payment_add_modal', compact('name', 'receipt_select', 'receipt_no_pay', 'ers', 'ers_sel', 'payments', 'vat'));
         }
         redirect();
-        //debug($ers);debug($receipt_select);debug($receipt_nopay);die;
     }
 
+    /**
+     * Функция строкового представления массива
+     * @param $data array входной массив
+     * @return string строка значений массива разделенных символом ;
+     */
+    public function prepareData($data) {
+        $num = '';                        // обнуляем переменную
+        foreach ($data as &$value) {
+            $num .= $value . ';';         // добавляем значение массива с символом ; в конце
+        }
+        return rtrim($num, ';'); // возвращаем строку без конечного знака ;
+    }
+
+    /**
+     * Функция записывающая в БД ЗО и вносящая исправления в приходы оплаченные этой ЗО
+     * @return void
+     */
     public function payReceiptAction() {
         // получаем данные пришедшие методом POST
         $pay_receipt = !empty($_POST) ? $_POST : null;
-        $num = '';                                      // обнуляем переменную
-        $receipt = $pay_receipt['receipt'];             // получаем массив приходов
-        foreach ($pay_receipt['receipt'] as &$value) {
-            $num .= $value . ';';                       // добавляем значение прихода (TOF00000278/2022;TOF00000278/2022;)
-        }
-        $num = rtrim($num, ';');            // убираем конечный знак ; получаем (TOF00000278/2022;TOF00000278/2022)
-        $pay_receipt['receipt'] = $num;     // записываем в сессию полученный результат
-        $num = '';                                      // обнуляем переменную
-        foreach ($pay_receipt['num_er'] as &$value) {
-            $num .= $value . ';';                       // добавляем номера ЕР (0009/122021ТЛТ;0008/092021ТЛТ;)
-        }
-        $num = rtrim($num, ';');            // убираем конечный знак ; получаем (0009/122021ТЛТ;0008/092021ТЛТ)
-        $pay_receipt['num_er'] = $num;      // записываем в сессию полученный результат
-        $num = '';                                      // обнуляем переменную
-        foreach ($pay_receipt['sum'] as &$value) {
-            $num .= $value . ';';                       // добавляем номера ЕР (2000.00;2000.00;)
-        }
-        $num = rtrim($num, ';');            // убираем конечный знак ; получаем (2000.00;2000.00)
-        $pay_receipt['sum'] = $num;      // записываем в сессию полученный результат
-        if (empty($pay_receipt['date_pay'])) {
-            $pay_receipt['date_pay'] = NULL;
-             //echo 'заменил на NULL';
-        }
-        // внесение изменений в приход        
+        // исправляем данные пришедшие в виде массива
+        $pay_receipt['receipt'] = $this->prepareData($pay_receipt['receipt']);
+        $pay_receipt['num_er'] = $this->prepareData($pay_receipt['num_er']);
+        $pay_receipt['sum'] = $this->prepareData($pay_receipt['sum']);
+        if (empty($pay_receipt['date_pay'])) $pay_receipt['date_pay'] = null;
+        // внесение изменений в ЗО
+        $pay = new Payment();
+        $pay->load($pay_receipt);
         if (empty($pay_receipt['id'])) {
-            //echo 'это новая ЗО';
-            $pay = new Payment();
-            $pay->load($pay_receipt);
+            // это новая ЗО
             $pay->save('payment'); 
         } else {
-            //echo 'это редактируемая ЗО';
-            $pay = new Payment();
-            $pay->load($pay_receipt);
+            // это редактируемая ЗО
             $pay->edit('payment', $pay_receipt['id']);            
         }
-        // внесение изменений в приход
-        $id = $this->editMy($receipt); // получаем массив ID приходов
-        foreach ($id as $k => $v) {
-            $edit_receipt['id'] = $v['id'];
-            $edit_receipt['date'] = $v['date'];
-            $edit_receipt['number'] = $v['number'];
-            $edit_receipt['sum'] = $v['sum'];            
-            $edit_receipt['type'] = $v['type'];
-            $edit_receipt['vat'] = $v['vat'];
-            $edit_receipt['partner'] = $v['partner'];
-            $edit_receipt['num_doc'] = $v['num_doc'];
-            $edit_receipt['date_doc'] = $v['date_doc'];
-            $edit_receipt['note'] = $v['note'];
+        // внесение изменений в приходы
+        $receipts = $this->getReceipts($pay_receipt['receipt']); // получаем массив ID приходов
+        foreach ($receipts as $value) {
+            $edit_receipt['id'] = $value['id'];
+            $edit_receipt['date'] = $value['date'];
+            $edit_receipt['number'] = $value['number'];
+            $edit_receipt['sum'] = $value['sum'];
+            $edit_receipt['type'] = $value['type'];
+            $edit_receipt['vat'] = $value['vat'];
+            $edit_receipt['partner'] = $value['partner'];
+            $edit_receipt['num_doc'] = $value['num_doc'];
+            $edit_receipt['date_doc'] = $value['date_doc'];
+            $edit_receipt['note'] = $value['note'];
             $edit_receipt['num_pay'] = dateYear($pay_receipt['number'], $pay_receipt['date']);
-            $edit_receipt['date_pay'] = $v['date_pay'];
+            $edit_receipt['date_pay'] = $value['date_pay'];
             $receipt = new Receipt();
             $receipt->load($edit_receipt);
             $receipt->edit('receipt', $edit_receipt['id']);
@@ -225,19 +236,23 @@ class ReceiptController extends AppController {
         redirect();        
     }
 
-    public function editMy($receipt) {
+    /**
+     * Функция возвращающая массив полных данных по приходам
+     * @param $receipt array Строка составных номеров приходов оплаченных данно ЗО
+     * @return array Полные данные о приходах
+     */
+    public function getReceipts($receipt) {
+        $receipts = []; // объявляем массив
         // проходимся по всем элементам массива
-        $id_receipt = [];
         foreach ($receipt as &$value) {
-            $num_receipt = $value; // номер прихода 00000000/2022
-            $num_receipt = explode('/', $num_receipt);
-            $number = $num_receipt[0]; // 00000000
-            $year = $num_receipt[1];   // 2022
-            // получаем  данные о приходе
+            $num_receipt = explode('/', $value); // получаем составной номер прихода
+            $number = $num_receipt[0]; // выделяем номер
+            $year = $num_receipt[1];   // выделяем год
+            // получаем полные данные о приходе
             $receipt_full = \R::findOne('receipt', "number = ? AND YEAR(date) = {$year}", [$number]);
-            $id_receipt[] = $receipt_full;
+            $receipts[] = $receipt_full;
         }
-        return $id_receipt;
+        return $receipts;
     }
 
 }
--- app/views/Receipt/receipt_add_modal.php
+++ app/views/Receipt/receipt_add_modal.php
@@ -1,51 +1,51 @@
 <div class="has-feedback">
     <label for="name">Наименование контрагента</label>
-    <input type="text" name="name" class="form-control" id="name" placeholder="Наименование КА" value="<?=isset($_SESSION['receipt']['partner']) ? h($_SESSION['receipt']['partner']) : '';?>" disabled>
+    <input type="text" name="name" class="form-control" id="name" placeholder="Наименование КА" value="<?= /** @var string $partner  Наименование КА */ $partner;?>" disabled>
 </div>
 <div class="has-feedback">
     <label for="type">Тип документа для оплаты</label>
     <select class="form-control" name="type" id="type">
         <option value="">Выберите тип</option>
-        <option value="ПТ" <?php if ($_SESSION['receipt']['type'] == 'ПТ') { echo ' selected';} ?>>Поступление товаров и услуг</option>
-        <option value="ЗП" <?php if ($_SESSION['receipt']['type'] == 'ЗП') { echo ' selected';} ?>>Заказ поставщику</option>
-        <option value="АО" <?php if ($_SESSION['receipt']['type'] == 'АО') { echo ' selected';} ?>>Авансовый отчет</option>
+        <option value="ПТ" selected>Поступление товаров и услуг</option>
+        <option value="ЗП">Заказ поставщику</option>
+        <option value="АО">Авансовый отчет</option>
     </select>
 </div>
 <div class="form-row">
     <div class="has-feedback col-6">
         <label for="date">Дата прихода</label>
-        <input type="date" name="date" class="form-control" id="date" placeholder="01.01.2021" value="<?=isset($_SESSION['receipt']['date']) ? h($_SESSION['receipt']['date']) : '';?>" required>
+        <input type="date" name="date" class="form-control" id="date" placeholder="01.01.2021" value="<?=date("Y-m-d");?>" required>
     </div>
     <div class="has-feedback col-6">
         <label for="number">Номер прихода</label>
-        <input type="text" name="number" class="form-control" id="number" placeholder="Номер" value="<?=isset($_SESSION['receipt']['number']) ? h($_SESSION['receipt']['number']) : '';?>" required>
+        <input type="text" name="number" class="form-control" id="number" placeholder="Номер" value="<?=null;?>" required>
     </div>
 </div>
 <div class="form-row">
     <div class="has-feedback col-6">
         <label for="sum">Сумма прихода</label>
-        <input type="number" name="sum" class="form-control" id="sum"  placeholder="" step="0.01" value="<?=isset($_SESSION['receipt']['sum']) ? h($_SESSION['receipt']['sum']) : '';?>" required>
+        <input type="number" name="sum" class="form-control" id="sum"  placeholder="" step="0.01" value="<?=null;?>" required>
     </div>
     <div class="has-feedback col-6">
         <label for="vat">НДС</label>
         <select class="form-control" name="vat" id="vat">
-            <option value="1.20" <?php if ($_SESSION['receipt']['vat'] == '1.20') { echo ' selected';} ?>>20%</option>
-            <option value="1.00" <?php if ($_SESSION['receipt']['vat'] == '1.00') { echo ' selected';} ?>>Без НДС</option>
+            <option value="1.20" <?php /** @var string $vat */ if ($vat == '1.20') { echo ' selected';} ?>>20%</option>
+            <option value="1.00" <?php if ($vat == '1.00') { echo ' selected';} ?>>Без НДС</option>
         </select>
     </div>
 </div>
 <div class="form-row">
     <div class="has-feedback col-6">
         <label for="num_doc">Номер вх.документа</label>
-        <input type="text" name="num_doc" class="form-control" id="num_doc" placeholder="Номер документа" value="<?=isset($_SESSION['receipt']['num_doc']) ? h($_SESSION['receipt']['num_doc']) : '';?>" required>
+        <input type="text" name="num_doc" class="form-control" id="num_doc" placeholder="Номер документа" value="<?=null;?>" required>
     </div>
     <div class="has-feedback col-6">
         <label for="date_doc">Дата вх.документа</label>
-        <input type="date" name="date_doc" class="form-control" id="date_doc" placeholder="" value="<?=isset($_SESSION['receipt']['date_doc']) ? h($_SESSION['receipt']['date_doc']) : '';?>" required>
+        <input type="date" name="date_doc" class="form-control" id="date_doc" placeholder="" value="<?=null;?>" required>
     </div>
 </div>
 <div class="has-feedback">
     <label for="note">Комментарий</label>
-    <input type="text" name="note" class="form-control" id="note" placeholder="Комментарий" value="<?=isset($_SESSION['receipt']['note']) ? h($_SESSION['receipt']['note']) : '';?>">
+    <input type="text" name="note" class="form-control" id="note" placeholder="Комментарий" value="<?=null;?>">
 </div>
-<input type="hidden" name="partner" value="<?=isset($_SESSION['receipt']['partner']) ? h($_SESSION['receipt']['partner']) : '';?>">
+<input type="hidden" name="partner" value="<?=$partner;?>">
