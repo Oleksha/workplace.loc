65f6652cd92407a4a609bc787d921d46fa8bafdd
Oleksha <43583583+Oleksha@users.noreply.github.com>
2022-03-31 13:16:19 +0400

    Добавлена текущая дата по умолчанию

--- app/views/Main/pay_add_date.php
+++ app/views/Main/pay_add_date.php
@@ -1,6 +1,6 @@
 <div class="has-feedback">
     <label for="date">Дата фактической оплаты</label>
-    <input type="date" name="date" class="form-control" id="date" placeholder="01.01.2021" required>
+    <input type="date" name="date" class="form-control" id="date" placeholder="01.01.2021" value="<?=date("Y-m-d");?>" required>
 </div>
 <input type="hidden" name="id" value="<?=isset($_SESSION['pay_date']['id']) ? h($_SESSION['pay_date']['id']) : '';?>">
 
