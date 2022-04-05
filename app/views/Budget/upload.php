2db67bbb26253dd3aa9b4cbb85456356289ab178
Oleksha <43583583+Oleksha@users.noreply.github.com>
2022-04-05 14:47:22 +0400

    Create upload.php

---
+++ app/views/Budget/upload.php
@@ -0,0 +1,20 @@
+<main role="main" class="flex-shrink-0">
+    <div class="container">
+        <div class="d-flex justify-content-between">
+            <h1 class="mt-1">Загрузка новых бюджетных операций</h1>
+        </div>
+
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="<?=PATH;?>">Главная</a></li>
+                <li class="breadcrumb-item "><a href="<?=PATH;?>/budget">Бюджетные операции</a></li>
+                <li class="breadcrumb-item active" aria-current="page">Загрузка данных</li>
+            </ol>
+        </nav>
+        
+        <form action="">
+            <input type="file">
+        </form>
+
+    </div>
+</main>
