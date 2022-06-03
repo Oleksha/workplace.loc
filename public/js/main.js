0bba7a0d85b71e44fb89e2d7d3356a3803968e49
Oleksha <43583583+Oleksha@users.noreply.github.com>
2022-06-03 13:17:53 +0400

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -191,48 +191,7 @@ function showEditKa(ka) {
     $('#editKAModal').modal();
 }
 
-$('body').on('click', '.add-receipt-link', function (e) {
-    e.preventDefault(); // отменяем действие по умолчанию для ссылки или кнопки
-    // получаем необходимые нам данные
-    let partner = $(this).data('name'), // наименование КА
-        vat = $(this).data('vat'); // идентификаиор КА
 
-    // отправляем стандартный аякс запрос на сервер
-    $.ajax({
-        url: '/receipt/add', // всегда указываем от корня
-        data: {vat: vat, partner: partner}, // передаем данные
-        type: 'GET', // тип передаваемого запроса
-        success: function (res) {
-            // если данные получены
-            showAddReceipt(res);
-        },
-        error: function () {
-            // если данных нет или запрос не дошел
-            alert('Ошибка получения данных с сервера! Попробуйте позже.');
-        }
-    });
-});
-
-$('body').on('click', '.edit-receipt-link', function (e) {
-    e.preventDefault(); // отменяем действие по умолчанию для ссылки или кнопки
-    // получаем необходимые нам данные
-    let id = $(this).data('id'); // идентификатор прихода
-
-    // отправляем стандартный аякс запрос на сервер
-    $.ajax({
-        url: '/receipt/edit', // всегда указываем от корня
-        data: {id: id}, // передаем данные
-        type: 'GET', // тип передаваемого запроса
-        success: function (res) {
-            // если данные получены
-            showEditReceipt(res);
-        },
-        error: function () {
-            // если данных нет или запрос не дошел
-            alert('Ошибка получения данных с сервера! Попробуйте позже.');
-        }
-    });
-});
 
 $('body').on('click', '.pay-receipt-link', function (e) {
     e.preventDefault(); // отменяем действие по умолчанию для ссылки или кнопки
@@ -256,16 +215,7 @@ $('body').on('click', '.pay-receipt-link', function (e) {
     });
 });
 
-function showAddReceipt(receipt) {
-    // выводим содержимое страницы
-    $('#addReceiptModal .modal-body').html(receipt);
-    $('#addReceiptModal').modal();
-}
-function showEditReceipt(receipt) {
-    // выводим содержимое страницы
-    $('#editReceiptModal .modal-body').html(receipt);
-    $('#editReceiptModal').modal();
-}
+
 function showPaymentReceipt(receipt) {
     // выводим содержимое страницы
     $('#payReceiptModal .modal-body').html(receipt);
@@ -463,4 +413,3 @@ function validateNumBO(str) {
         }
 
     });*/
-
